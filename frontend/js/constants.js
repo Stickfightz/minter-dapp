19c11b8ee90d2cc5ace1ee9a9afac07aae55b6cd
yanen4 <yanenjustin40@gmail.com>
2022-04-14 22:54:25 +0200

    rinke

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xbD87cF9780D7A57a0D0d7fFe2DE49376676B0fA7"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
